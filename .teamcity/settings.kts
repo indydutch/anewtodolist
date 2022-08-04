6f3551e11a6f6bd6d82d6821db42eef32d870b31
indydutch <indydutch@TeamCity>
2022-08-04 16:09:09 -0400

    TeamCity change in 'Anewtodolist' project: parameters of 'Step 2' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -70,6 +70,11 @@ object Build : BuildType({
 object Step2 : BuildType({
     name = "Step 2"
 
+    params {
+        text("Confirmation", "", description = "WARNING: This will upload the installers to the web site. Please review the Dependencies tab for what versions will be published.", display = ParameterDisplay.PROMPT,
+              regex = """\biamauthorizedtorunthisbuild\b""", validationMessage = "Please contact the product director before continuing.")
+    }
+
     vcs {
         root(HttpsGithubComIndydutchCheckBuildSequences1git)
     }
